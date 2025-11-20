d9abd90bd0a8fe6d811771d4aeb24956e41f8f27
Danail Donkov <danail.donkov5@gmail.com>
2025-11-20 18:03:59 +0200

    Update trick.cpp

--- Winter_Tournament/trick.cpp
+++ Winter_Tournament/trick.cpp
@@ -1,1 +1,53 @@
+#include <iostream>
+#include <stack>
+using namespace std;
 
+int main()
+{
+	stack <char> symbols;
+	stack <int> numbers;
+
+	int N;
+	char S;
+	int current_number;
+
+	do
+	{
+		cin >> S;
+		cin >> N;
+		numbers.push(N);
+		symbols.push(S);
+	} while (symbols.top() != '=');
+	
+
+	current_number = numbers.top();
+	numbers.pop();
+	symbols.pop();
+
+
+	while (numbers.empty() == false)
+	{
+		switch (symbols.top())
+		{
+		case '+':
+			current_number -= numbers.top();
+			break;
+
+		case '-':
+			current_number += numbers.top();
+			break;
+
+		case '*':
+			current_number /= numbers.top();
+			break;
+
+		case '/':
+			current_number /= numbers.top();
+			break;
+		}
+		numbers.pop();
+		symbols.pop();
+	}
+	cout << current_number << endl;
+	return 0;
+}
