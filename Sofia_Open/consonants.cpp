04b59d46ec46c9b6adc06e5a33c1e2b56bd71b22
Danail Donkov <danail.donkov5@gmail.com>
2025-11-27 15:39:54 +0200

    Update consonants.cpp

--- Sofia_Open/consonants.cpp
+++ Sofia_Open/consonants.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main()
+{
+	string text;
+	cin >> text;
+
+	string vowels = "aeiouyAEIOUY";
+	int current = 0;
+	int best = 0;
+
+	for (char c : text)
+	{
+		if (vowels.find(c) == string::npos)
+		{
+			current++;
+			if (current > best)
+			{
+				best = current;
+			}
+		}
+		else {
+			current = 0;
+		}
+	}
+	cout << best;
+	return 0;
+}
