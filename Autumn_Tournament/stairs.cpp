3142c15b2446e1719a0a424f89edd0d2081ec8f7
Danail Donkov <danail.donkov5@gmail.com>
2025-11-27 15:16:43 +0200

    Update stairs.cpp

--- Autumn_Tournament/stairs.cpp
+++ Autumn_Tournament/stairs.cpp
@@ -1,37 +1,51 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-	int N_stone_bricks;
-	int K_milimetres;
-	int S_count_stairs = 0;
+    int N_stone_bricks;
+    int K_milimetres;
+    int S_count_stairs = 0;
 
-	vector <int> stairs(N_stone_bricks);
+    cin >> N_stone_bricks;
+    cin >> K_milimetres;
 
-	cin >> N_stone_bricks;
-	cin >> K_milimetres;
+    vector<int> stairs(N_stone_bricks);
 
+    for (int i = 0; i < N_stone_bricks; i++)
+    {
+        cin >> stairs[i];
+    }
 
-	for (int i = 0; i < N_stone_bricks; i++)
-	{
-		cin >> stairs[i];
-	}
+    
+    sort(stairs.begin(), stairs.end());
 
+    int current_stairs = 1;
+    S_count_stairs = 1;
 
-	int total_length = 0;
-	for (int i = 0; i < N_stone_bricks; i++)
-	{
-		total_length += stairs[i];
-	}
-	cout << total_length << endl;
-	if (total_length == K_milimetres)
-	{
-		cout << N_stone_bricks << endl;
-	} else {
-		return 0;
-	}
+    for (int i = 1; i < N_stone_bricks; i++)
+    {
+        int diff = stairs[i] - stairs[i - 1];
 
+        if (diff > 0 && diff <= K_milimetres)
+        {
+            current_stairs++;
+        }
+        else
+        {
+            current_stairs = 1;
+        }
+
+        if (current_stairs > S_count_stairs)
+        {
+            S_count_stairs = current_stairs;
+        }
+    }
+
+    cout << S_count_stairs << endl;
+
+    return 0;
 }
