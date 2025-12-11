f2fd2ec4d5a4af2e40e738d5417506d018f47976
Danail Donkov <danail.donkov5@gmail.com>
2025-12-11 18:07:42 +0200

    Update and rename eratosthen.cpp to eratosthen_count_primes.cpp

---
+++ National_Olympiad/eratosthen_count_primes.cpp
@@ -0,0 +1,40 @@
+/*Програма за извеждане на простите числа по алгоритъм на Ерапстен.*/
+
+/*Започваме от p*p, защото всички по-малки, кратни на p,
+вече са били елиминирани от по-малките прости числа.*/
+
+/*vector<int> sieve(int n)
+	{
+		vector<bool> prime(n + 1, true);
+		
+		for (int p = 2; p * p <= n; p++)
+		{
+			if (prime[p] == true)
+			{
+				for (int i = p * p; i <= n; i += p)
+					prime[i] = false;
+			}
+		}
+
+		vector<int> result;
+		for (int p = 2; p <= n; p++)
+		{
+			if (prime[p])
+			{
+				result.push_back(p);
+			}
+		}
+		return result;
+	}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> result = sieve(n);
+	for (int i = 0; i < result.size(); i++)
+	{
+		cout << result[i] << endl;
+	}
+	return 0;
+}
